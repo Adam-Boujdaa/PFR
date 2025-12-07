cf8040fec6053788b697663ba7916d6654a5f48d
Jean <Jeandavy1812@gmail.com>
2025-12-07 19:32:13 +0100

    Correction

--- module_simulation/python/simulation.py
+++ module_simulation/python/simulation.py
@@ -32,13 +32,13 @@ class RobotSimulation:
         #Création des éléments
 
         #Création de la fenêtre
-        self.screen = turtle.Screen()
+        self.screen = tl.Screen()
         self.screen.setup(width=w_width, height=w_height)
         self.screen.title(w_title)
         self.screen.bgcolor(bg_color)
 
         #Création du robot
-        self.robot = turtle.Turtle()
+        self.robot = tl.Turtle()
         self.robot.shape(rb_shape)
         self.robot.color(rb_color)
         self.robot.shapesize(rb_size, rb_size)
@@ -47,34 +47,3 @@ class RobotSimulation:
         self.X = 0
         self.y = 0
         self.theta = 0 #angle
-
-        print("Simulation initialisée")
-        print("Echelle : 1m = {self.scale} pixels")
-
-        def _load_config(self, config_file):
-            #Permet de charger le fichier de configuration
-            if not os.path.exists(config_file):
-                raise FileNotFoundError(f"Fichier de configuration introuvable: {config_file}")
-
-            config = confp.ConfigParser()
-            config.read(config_file)
-            print(f"La configuration a bien été chargée depuis : {config_file}")
-            return config
-
-        #Commande de base
-
-        #Avance
-        def forward(self, distance):
-            pixels = distance * self.scale
-            self.robot.forward(pixels)
-            print(f"Robot avance de {distance}m")
-
-        #Tests
-        if __name__ == "__main__":
-            print("Démarrage de la simulation")
-            sim = RobotSimulation()
-
-            #Test1 : Déplacement de base
-            sim.forward(2)
-
-            input("Touche 'Entrée' pour fermer")
