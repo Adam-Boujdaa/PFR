0d1b0f28fa5b9d89c2762df54844e94744b4e4a2
jeand <jeand@device-jeand.home>
2026-01-04 11:51:39 +0100

    Ajout lecture fichier de configuration

--- module_simulation/python/simulation.py
+++ module_simulation/python/simulation.py
@@ -7,27 +7,61 @@ import turtle as tl
 
 class RobotSimulation:
     
-    def __init__(self):
+    # explication self : # (https://www.geeksforgeeks.org/python/self-in-python-class/)
+    # lecture fichier config (https://zestedesavoir.com/tutoriels/2514/un-zeste-de-python/6-entrees-sorties/2-lecture-fichier/)
+    def load_config(self, fichier_conf):
+        config = {}
+        try:
+            with open(fichier_conf, 'r') as file: # 'r' = mode read
+                for line in file:
+                    line = line.strip()
+                    if line and not line.startswith('#'):
+                        if '=' in line:
+                            key, value = line.split('=',1)
+                            config[key.strip()] = value.strip()
+            print("Fichier de configuration chargé")
+        except FileNotFoundError:
+            print("Fichier de configuration ", fichier_conf, "introuvable.")
+        return config
+    def __init__(self, fichier_conf="config/simulation.txt"):
         print("Lancement de la simulation")
         
+        # chargement config
+        self.config = self.load_config(fichier_conf)
+
+        # récupération paramètres
+
         # fenêtre
-        self.screen = tl.Screen()
-        self.screen.setup(width=800, height=600)
-        self.screen.title("Simulation Robot PFR1")
+        f_width = int(self.config.get('WINDOW_WIDTH', 800))
+        f_height = int(self.config.get('WINDOW_HEIGHT', 600))
+        f_title = self.config.get('WINDOW_TITLE', 'Simulation Robot')
+        bg_color = self.config.get('BACKGROUND_COLOR', 'white')
         
+        # échelle
+        self.scale = int(self.config.get('SCALE', 100))
+
         # robot
+        r_shape = self.config.get('ROBOT_SHAPE', 'triangle')
+        r_color = self.config.get('ROBOT_COLOR', 'blue')
+        r_size = float(self.config.get('ROBOT_SIZE', 1.5))
+
+        # création fenêtre
+        self.screen = tl.Screen()
+        self.screen.setup(width=f_width, height=f_height)
+        self.screen.title(f_title)
+        self.screen.bgcolor(bg_color)
+
+        # création orbot
         self.robot = tl.Turtle()
-        self.robot.shape("triangle")
-        self.robot.color("blue")
+        self.robot.shape(r_shape)
+        self.robot.color(r_color)
+        self.robot.shapesize(r_size, r_size)
 
         # position robot
         self.x = 0.0
         self.y = 0.0
         self.alpha = 0.0
 
-        # echelle
-        self.scale = 100
-        
         print("Simulation initialisée")
 
     # fonctions de base
