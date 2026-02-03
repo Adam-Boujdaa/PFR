144428ae39a509d6fd4ce93841f4fadcd17faab4
chentirhakim2-boop <chentir.hakim2@gmail.com>
2026-02-03 22:13:37 +0100

    Update log.c

--- src/log.c
+++ src/log.c
@@ -1,3 +1,21 @@
+/* ============================================================
+   Fichier  : log.c
+   Projet   : PFR1 – Projet Fil Rouge
+   Auteur   : CHENTIR Hakim
+   ------------------------------------------------------------
+                        DESCRIPTION
+   ------------------------------------------------------------
+   Ce fichier gère les logs de l'application.
+   Chaque action importante est enregistrée dans le fichier
+   "logs.txt" avec la date et l'heure.
+   ------------------------------------------------------------
+                        RÔLE DU MODULE
+   ------------------------------------------------------------
+   mémoriser les actions de l'utilisateur
+   aider à comprendre le fonctionnement du programme
+   permettre à l'administrateur de consulter l'historique
+   ============================================================ */
+
 #include <stdio.h>
 #include <time.h>
 #include "log.h"
