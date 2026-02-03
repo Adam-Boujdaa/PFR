e4a53a2339fccaf4a0074d551d086f5b2c1ad44e
chentirhakim2-boop <chentir.hakim2@gmail.com>
2026-02-03 22:13:59 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+/* ============================================================
+   Fichier  : main.c
+   Projet   : PFR1 – Projet Fil Rouge
+   Auteur   : CHENTIR Hakim
+   ------------------------------------------------------------
+                        DESCRIPTION
+   ------------------------------------------------------------
+   Ce fichier contient le point d'entrée du programme.
+   Il initialise l'application en appelant la sélection de
+   langue puis lance le menu principal de l'interface.
+   ------------------------------------------------------------
+                        RÔLE DU MODULE
+   ------------------------------------------------------------
+   démarrer l'application
+   initialiser la langue de l'interface
+   lancer le menu principal de navigation
+   ============================================================ */
+
 #include "menu.h"
 #include "module_image.h"
 
